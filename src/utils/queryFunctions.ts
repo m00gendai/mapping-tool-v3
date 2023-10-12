6e12fa2b33afbfd1b5a4d9c61e48ae030a4c322a
Marcel Weber <mrweber@gmx.ch>
2023-10-12 14:36:07 +0200

    Added Navaid query function Changed from the v2 version as the check for single or multiple navaids was redundant

--- src/utils/queryFunctions.ts
+++ src/utils/queryFunctions.ts
@@ -1,6 +1,8 @@
 import { calcDegToDec } from "./conversions"
 import {airports} from "../EAD_Data/EAD_AD_ALL"
+import {navaids} from "../EAD_Data/EAD_NAV_ALL"
 
+// RETURNS QUERY RESULTS FOR COORDINATES
 export function placeCoords(coordinatesValue:string){
     const coordsArray: string[] = coordinatesValue.split(/\s+/g) // s+ is one or more whitespace characters
     const zeroZeroCoords:string[][] = [] // initialises normalized array of string coordinates (normalized to 6 and 7 characters)
@@ -38,13 +40,14 @@ export function placeCoords(coordinatesValue:string){
     return returnCoordinates
 }
 
+// RETURNS QUERY RESULT FOR LOCATION INDICATOR
 export function placeLoci(lociValue:string){
     const newAirports = airports.map(data => {return [
         data.codeId, 
         data.geoLat.charAt(data.geoLat.length-1) == "N" ? data.geoLat.substring(0, data.geoLat.length-1) : `-${data.geoLat.substring(0, data.geoLat.length-1)}`,  
         data.geoLong.charAt(data.geoLong.length-1) == "E" ? data.geoLong.substring(0, data.geoLong.length-1) : `-${data.geoLong.substring(0, data.geoLong.length-1)}`,
         data.txtName]}) 
-    const newAirportCodes = newAirports.map(code => {return code[0]}) // makes a one dimensional array just with airport codes
+    const newAirportCodes:string[] = newAirports.map(code => {return code[0]}) // makes a one dimensional array just with airport codes
     const multiLocis:string[][] = []
     const multiPorts = lociValue.toUpperCase().split(" ") // splits the input separated by space and makes an array
     multiPorts.forEach(multiPort => { // for every searched loci...
@@ -60,4 +63,32 @@ export function placeLoci(lociValue:string){
     }
     
     return multiLocis
+}
+
+// RETURNS QUERY RESULT FOR NAVAID
+export function placeNavaid(navaidValue:String){
+
+    const newNavaids = navaids.map(data => {return [
+        data.codeId, 
+        data.geoLat.charAt(data.geoLat.length-1) == "N" ? data.geoLat.substring(0, data.geoLat.length-1) : `-${data.geoLat.substring(0, data.geoLat.length-1)}`,  
+        data.geoLong.charAt(data.geoLong.length-1) == "E" ? data.geoLong.substring(0, data.geoLong.length-1) : `-${data.geoLong.substring(0, data.geoLong.length-1)}`,
+        data.txtName ? data.txtName : "",
+        data.codeType ? data.codeType : "DME or TACAN"]})
+    const newNavaidCodes:String[] = newNavaids.map(code => {return code[0]})
+
+        let multiNavs:string[][] = []
+        const multiAids = navaidValue.toUpperCase().split(" ")
+        multiAids.forEach(multiAid => {
+            for(const navaid of newNavaids){
+                if(multiAid.toUpperCase() == navaid[0].toUpperCase()){
+                    multiNavs.push([navaid[1], navaid[2], `${navaid[0]} ${navaid[4]}<br>${navaid[3]}`])
+                }
+            }
+        })
+        const unknownNavaids = multiAids.filter(navaid => { return newNavaidCodes.indexOf(navaid) == -1 })
+        if(unknownNavaids.length > 0){
+            alert(`Navaids ${unknownNavaids.join(" ")} not found`)
+        }
+        return multiNavs
+    
 }
