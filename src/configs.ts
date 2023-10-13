69ef7d130d848569e4821795f62ab2dcbb1debed
Marcel Weber <mrweber@gmx.ch>
2023-10-13 11:42:17 +0200

    Added configs file As for now only custom icon

---
+++ src/configs.ts
@@ -0,0 +1,14 @@
+import L from "leaflet"
+
+export function createIcon(type:string){
+    const icon = L.icon({
+        iconUrl: `/marker_${type}.png`,
+        iconSize: [38, 38],
+        iconAnchor: [14, 38],
+        popupAnchor: [0, -40],
+        shadowUrl: '',
+        shadowSize: [68, 95],
+        shadowAnchor: [22, 94]
+    })
+    return icon
+}
