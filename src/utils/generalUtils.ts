8dd4f266426dcf328b481b05443bf76f6c74c469
Marcel Weber <mrweber@gmx.ch>
2023-10-17 01:18:29 +0200

    Moved table build function to here

--- src/utils/generalUtils.ts
+++ src/utils/generalUtils.ts
@@ -2,6 +2,7 @@ import L from "leaflet"
 import arc from "arc"
 import "leaflet-arc"
 import LatLon from 'geodesy/latlon-ellipsoidal-vincenty.js'
+import { State } from "../interfaces"
 
 // This generates the coordinate array needed for great circle aware polylines
 export function generateArcLine(polylineMarkerArray: L.Marker[]){
@@ -35,4 +36,64 @@ export function calculateDist(depLat:number, depLng:number, destLat:number, dest
     const p1 = new LatLon(depLat, depLng)
     const p2 = new LatLon(destLat, destLng)
     return p1.distanceTo(p2)
+}
+
+export function buildTable(polylineMarkerArray:L.Marker[], state:State){
+    const lineFeed: HTMLTableRowElement = document.createElement("tr")
+      lineFeed.className="polylineField_lineFeed"
+      document.getElementById("polylineField_table_body")!.appendChild(lineFeed)
+
+      const tdDep:HTMLTableCellElement = document.createElement("td")
+      const tdDist:HTMLTableCellElement = document.createElement("td")
+      const tdDest:HTMLTableCellElement = document.createElement("td")
+      const tdTime:HTMLTableCellElement = document.createElement("td")
+
+      tdTime.className = "polylineField_table_body_time"
+      tdTime.id= `polylineField_table_body_time_${state.markerClicks}`
+      const tdTotalDist:HTMLTableCellElement = document.createElement("td")
+      const tdTotalTime:HTMLTableCellElement = document.createElement("td")
+      lineFeed.appendChild(tdDep)
+      lineFeed.appendChild(tdDist)
+      lineFeed.appendChild(tdDest)
+      lineFeed.appendChild(tdTime)
+      lineFeed.appendChild(tdTotalDist)
+      lineFeed.appendChild(tdTotalTime)
+      state.setDep = `${polylineMarkerArray[polylineMarkerArray.length-2].getPopup()!.getContent()!.toString().split("<br>")[0]}`
+      tdDep.innerText = state.setDep
+      state.setDist = [...state.setDist, (calculateDist(
+        polylineMarkerArray[polylineMarkerArray.length-2].getLatLng().lat, 
+        polylineMarkerArray[polylineMarkerArray.length-2].getLatLng().lng, 
+        polylineMarkerArray[polylineMarkerArray.length-1].getLatLng().lat, 
+        polylineMarkerArray[polylineMarkerArray.length-1].getLatLng().lng, 
+        )/1852)]
+      tdDist.innerText = state.setDist[state.markerClicks].toFixed(2)
+      state.setDest = `${polylineMarkerArray[polylineMarkerArray.length-1].getPopup()!.getContent()!.toString().split("<br>")[0]}`
+      const time = state.setDist[state.markerClicks-1]/state.setSpeed
+      const n = new Date(0,0);
+      n.setSeconds(+time * 60 * 60);
+      state.setTotalTime = state.setTotalTime + time
+      console.log(state.setTotalTime)
+      tdTime.innerText = n.toTimeString().slice(0, 8)
+      tdDest.innerText = state.setDest
+      state.totalDistance = state.totalDistance + (calculateDist(
+        polylineMarkerArray[polylineMarkerArray.length-2].getLatLng().lat, 
+        polylineMarkerArray[polylineMarkerArray.length-2].getLatLng().lng, 
+        polylineMarkerArray[polylineMarkerArray.length-1].getLatLng().lat, 
+        polylineMarkerArray[polylineMarkerArray.length-1].getLatLng().lng, 
+        )/1852)
+      tdTotalDist.innerText  = `${(state.totalDistance).toFixed(2)}NM`
+      const m = new Date(0,0)
+      m.setSeconds(+state.setTotalTime * 60 * 60)
+        tdTotalTime.innerText = m.toTimeString().slice(0, 8)
+
+      const timeFields:NodeList = document.querySelectorAll(".polylineField_table_body_time")
+    timeFields.forEach((timeField, index) =>{
+      const time = state.setDist[index]/state.setSpeed
+      const n = new Date(0,0);
+      n.setSeconds(+time * 60 * 60);
+      const htmlTimeField = timeField as HTMLElement
+      htmlTimeField.innerText = n.toTimeString().slice(0, 8)
+    })
+      state.setTimeFields = state.setTimeFields + 1
+
 }
