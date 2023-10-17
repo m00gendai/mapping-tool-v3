6eb2191b1819c2492451d1951dcff0d121581487
Marcel Weber <mrweber@gmx.ch>
2023-10-17 17:35:41 +0200

    Layers file created This basically returns a layer object dynamically and contains itself a few helper functions. This is why it is separate and not part of generalUtils

---
+++ src/layers.ts
@@ -0,0 +1,42 @@
+import * as EB from "./Layer_Data/EB.json"
+import * as LD from "./Layer_Data/LD.json"
+import { GeoJsonObject, GeoJsonProperties } from "geojson";
+import { LayerGroup_layer } from "./interfaces"
+
+function getCountry(string:string){
+    if(string === "EB"){
+        return EB as GeoJsonObject
+    }
+    if(string === "LD"){
+        return LD as GeoJsonObject
+    }
+}
+function getColor(type:string){
+    if(type === "TMA"){
+        return "blue"
+    }
+    if(type ==="CTR"){
+        return "gold"
+    }
+    if(type==="FIR"){
+        return "magenta"
+    }
+    return "black"
+}
+
+export function getLayer(layer:LayerGroup_layer){
+    const country:GeoJsonObject | undefined = getCountry(layer.id.substring(0,2))
+    const type:string = layer.id.substring(4,)
+/* 
+This is absolutely ridiculous, hacky and sketchy as all hell, but it works.
+Judging how wonky Leaflet is with npm and especially TypeScript, I'll take it, though.
+*/
+/*@ts-expect-error */
+return L.GeoJSON = L.geoJSON(country, {style: {color: getColor(type)}, filter: function(feature:GeoJsonProperties, layer:L.Layer) {
+    if(feature?.properties.Type == type){
+        return feature.properties.Type
+    }}}).bindTooltip(function(layer){
+      /*@ts-expect-error */
+      return `${layer.feature.properties.ICAO}<br>${layer.feature.properties.Name}`
+    },{sticky: true})
+}
