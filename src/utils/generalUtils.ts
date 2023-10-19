798514c5e20ccf63238db5ee71d912f163f8b932
Marcel Weber <mrweber@gmx.ch>
2023-10-19 14:48:29 +0200

    ZoomIn zoomOut SVG handling

--- src/utils/generalUtils.ts
+++ src/utils/generalUtils.ts
@@ -52,6 +52,12 @@ export function createSVG(flag:string, state:State){
   if(flag === "sidebarToggle_right"){
     return `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9 6L15 12L9 18" stroke="${state.darkmode ? '#fff' : '#000'}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`
   }
+  if(flag === "zoomIn"){
+    return `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12H20M12 4V20" stroke="${state.darkmode ? '#fff' : '#000'}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`
+  }
+  if(flag === "zoomOut"){
+    return `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12L18 12" stroke="${state.darkmode ? '#fff' : '#000'}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`
+  }
   return ""
 }
 
